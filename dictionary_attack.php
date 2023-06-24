bf413a11506823cf391f8d5daba2a5dd714b6ce2
fatougaye08 <123908779+fatougaye08@users.noreply.github.com>
2023-06-24 17:34:11 -0400

    Add files via upload

--- dictionary_attack.php
+++ dictionary_attack.php
@@ -1,36 +1,25 @@
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  if (isset($_POST['password'])) {
-    $password = $_POST['password'];
-
-    // Charge le dictionnaire depuis un fichier texte
-    $dictionaryPasswords = file('dictionnary.txt', FILE_IGNORE_NEW_LINES);
-
-    // Vérifie si le mot de passe se trouve dans le dictionnaire
-    if (in_array($password, $dictionaryPasswords)) {
-      $response = array('found' => true, 'password' => $password);
-    } else {
-      $response = array('found' => false, 'password' => null);
-    }
-
-    // Renvoie la réponse en tant que JSON
-    header('Content-Type: application/json');
-    echo json_encode($response);
-  } else {
-    // Gérer le cas où le champ de mot de passe n'a pas été soumis
-    $response = array('found' => false, 'password' => null);
-
-    // Renvoie la réponse en tant que JSON
-    header('Content-Type: application/json');
-    echo json_encode($response);
-  }
-} else {
-  // Gérer le cas où la méthode de requête n'est pas POST
+// Récupère le mot de passe soumis depuis la requête
+$password = $_POST['password'];
+if (empty($password)) {
   $response = array('found' => false, 'password' => null);
-
-  // Renvoie la réponse en tant que JSON
-  header('Content-Type: application/json');
-  echo json_encode($response);
+} elseif (in_array($password, $dictionaryPasswords)) {
+  $response = array('found' => true, 'password' => $password);
+} else {
+  $response = array('found' => false, 'password' => null);
 }
+
+// Charge le dictionnaire depuis un fichier texte
+$dictionaryPasswords = file('dictionnary.txt', FILE_IGNORE_NEW_LINES);
+
+// Vérifie si le mot de passe se trouve dans le dictionnaire
+if (in_array($password, $dictionaryPasswords)) {
+  $response = array('found' => true, 'password' => $password);
+} else {
+  $response = array('found' => false, 'password' => null);
+}
+
+// Renvoie la réponse en tant que JSON
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
